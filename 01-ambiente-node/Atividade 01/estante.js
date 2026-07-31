9516c1d88924f2631bbe3775bfb6fc458ac23ae9
Luis Silva <luis.m.silva8@edu.senai.br>
2026-07-31 14:59:55 -0300

    feat: adiciona atividade minha estante

---
+++ 01-ambiente-node/Atividade 01/estante.js
@@ -0,0 +1,15 @@
+const livro1 = {
+  titulo: "Clean Code",
+  autor: "Robert C. Martin",
+  preco: 89.9,
+  estoque: 12,
+};
+
+const livro2 = {
+  titulo: "Arquitetura Limpa",
+  autor: "Robert C. Martin",
+  preco: 76.9,
+  estoque: 5,
+};
+
+module.exports = { livro1, livro2 };
